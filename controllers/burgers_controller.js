cbf31e5b0840b2d3186b8b0b4654c2c106044471
Randall Phillips <rphill45@uncc.edu>
2020-02-16 22:11:08 -0500

    adding public files from other burger app

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,16 +1,13 @@
 var express = require("express");
 var router = express.Router();
 
-// Import the model (burger.js) to use its database functions
 var burger = require("../models/burger.js");
 
-// Create the router for the app
 router.post("/", function(req, res) {
   burger.insertOne(
     ["burger_name", "devoured"],
     [req.body.burger_name, req.body.devoured],
     function(result) {
-      // Send back the ID of the new burger
       res.json({ id: result.insertId });
     }
   );
@@ -28,7 +25,6 @@ router.put("/:id", function(req, res) {
     condition,
     function(result) {
       if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
       } else {
         res.status(200).end();
@@ -37,5 +33,4 @@ router.put("/:id", function(req, res) {
   );
 });
 
-// Export routes for server.js to use.
 module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,22 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+  selectAll: function(callback) {
+    orm.selectAll("burgers", function(result) {
+      callback(result);
+    });
+  },
+  // The variables cols and vals are arrays.
+  insertOne: function(cols, vals, callback) {
+    orm.insertOne("burgers", cols, vals, function(result) {
+      callback(result);
+    });
+  },
+  updateOne: function(objColVals, condition, callback) {
+    orm.updateOne("burgers", objColVals, condition, function(result) {
+      callback(result);
+    });
+  }
+};
+
+module.exports = burger;
