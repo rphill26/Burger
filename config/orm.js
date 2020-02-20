ccdb63925b091069ad7937c4c7ea228726a28a50
Randall Phillips <rphill45@uncc.edu>
2020-02-20 16:45:00 -0500

    fixing heroku deploy

--- config/orm.js
+++ config/orm.js
@@ -1,63 +1,49 @@
-// Import MySQL connection.
-var connection = require("../config/connection.js");
+const connection = require("../config/connection.js");
 
-// Create the methods that will execute the necessary MySQL commands in the controllers.
-// These are the methods you will need to use in order to retrieve and store data in your database.
+// function printQuestionMarks(num) {
+//   const arr = [];
 
-// Helper function for SQL syntax.
-// ["?", "?", "?"].toString() => "?,?,?";
-function printQuestionMarks(num) {
-  var arr = [];
+//   for (var i = 0; i < num; i++) {
+//     arr.push("?");
+//   }
 
-  for (var i = 0; i < num; i++) {
-    arr.push("?");
-  }
+//   return arr.toString();
+// }
 
-  return arr.toString();
-}
-
-// Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
-  var arr = [];
+  const arr = [];
 
-  // loop through the keys and push the key/value as a string int arr
   for (var key in ob) {
     var value = ob[key];
-    // check to skip hidden properties
     if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (big mac => 'big mac')
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      // e.g. {burger_name: 'big mac'} => ["burger_name='big mac'"]
-      // e.g. {devoured: true} => ["devoured=true"]
       arr.push(key + "=" + value);
     }
   }
-
-  // translate array of strings to a single comma-separated string
   return arr.toString();
 }
 
-// Object for SQL statement functions
-var orm = {
-  selectAll: function(tableInput, callback) {
+// Object for all our SQL statement functions.
+const orm = {
+  all: function(tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
     connection.query(queryString, function(err, result) {
       if (err) {
         throw err;
       }
-      callback(result);
+      cb(result);
     });
   },
-  insertOne: function(table, cols, vals, callback) {
+  create: function(table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
     queryString += " (";
     queryString += cols.toString();
     queryString += ") ";
     queryString += "VALUES (";
-    queryString += printQuestionMarks(vals.length);
+    // queryString += printQuestionMarks(vals.length);
     queryString += ") ";
 
     console.log(queryString);
@@ -66,11 +52,11 @@ var orm = {
       if (err) {
         throw err;
       }
-      callback(result);
+
+      cb(result);
     });
   },
-  // EX objColVals would be {burger_name: big mac, devoured: true}
-  updateOne: function(table, objColVals, condition, callback) {
+  update: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
     queryString += " SET ";
@@ -79,15 +65,27 @@ var orm = {
     queryString += condition;
 
     console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  delete: function(table, condition, cb) {
+    var queryString = "DELETE FROM " + table;
+    queryString += " WHERE ";
+    queryString += condition;
 
     connection.query(queryString, function(err, result) {
       if (err) {
         throw err;
       }
-      callback(result);
+
+      cb(result);
     });
   }
 };
 
-// Export the orm object for the model (burger.js).
 module.exports = orm;
