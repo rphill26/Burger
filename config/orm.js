a6c830cc2b6d6e1157dbead5bc69e6914071cf91
Randall Phillips <rphill45@uncc.edu>
2020-02-21 12:48:39 -0500

    fixing heroku deploy

--- config/orm.js
+++ config/orm.js
@@ -1,49 +1,63 @@
-const connection = require("../config/connection.js");
+// Import MySQL connection.
+var connection = require("../config/connection.js");
 
-// function printQuestionMarks(num) {
-//   const arr = [];
+// Create the methods that will execute the necessary MySQL commands in the controllers.
+// These are the methods you will need to use in order to retrieve and store data in your database.
 
-//   for (var i = 0; i < num; i++) {
-//     arr.push("?");
-//   }
+// Helper function for SQL syntax.
+// ["?", "?", "?"].toString() => "?,?,?";
+function printQuestionMarks(num) {
+  var arr = [];
 
-//   return arr.toString();
-// }
-
-function objToSql(ob) {
-  const arr = [];
-
-  for (var key in ob) {
-    var value = ob[key];
-    if (Object.hasOwnProperty.call(ob, key)) {
-      if (typeof value === "string" && value.indexOf(" ") >= 0) {
-        value = "'" + value + "'";
-      }
-      arr.push(key + "=" + value);
-    }
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
   }
+
   return arr.toString();
 }
 
-// Object for all our SQL statement functions.
-const orm = {
-  all: function(tableInput, cb) {
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+  var arr = [];
+
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (big mac => 'big mac')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      // e.g. {burger_name: 'big mac'} => ["burger_name='big mac'"]
+      // e.g. {devoured: true} => ["devoured=true"]
+      arr.push(key + "=" + value);
+    }
+  }
+
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+}
+
+// Object for SQL statement functions
+var orm = {
+  selectAll: function(tableInput, callback) {
     var queryString = "SELECT * FROM " + tableInput + ";";
     connection.query(queryString, function(err, result) {
       if (err) {
         throw err;
       }
-      cb(result);
+      callback(result);
     });
   },
-  create: function(table, cols, vals, cb) {
+  insertOne: function(table, cols, vals, callback) {
     var queryString = "INSERT INTO " + table;
 
     queryString += " (";
     queryString += cols.toString();
     queryString += ") ";
     queryString += "VALUES (";
-    // queryString += printQuestionMarks(vals.length);
+    queryString += printQuestionMarks(vals.length);
     queryString += ") ";
 
     console.log(queryString);
@@ -52,11 +66,11 @@ const orm = {
       if (err) {
         throw err;
       }
-
-      cb(result);
+      callback(result);
     });
   },
-  update: function(table, objColVals, condition, cb) {
+  // EX objColVals would be {burger_name: big mac, devoured: true}
+  updateOne: function(table, objColVals, condition, callback) {
     var queryString = "UPDATE " + table;
 
     queryString += " SET ";
@@ -65,27 +79,15 @@ const orm = {
     queryString += condition;
 
     console.log(queryString);
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-  },
-  delete: function(table, condition, cb) {
-    var queryString = "DELETE FROM " + table;
-    queryString += " WHERE ";
-    queryString += condition;
 
     connection.query(queryString, function(err, result) {
       if (err) {
         throw err;
       }
-
-      cb(result);
+      callback(result);
     });
   }
 };
 
+// Export the orm object for the model (burger.js).
 module.exports = orm;
